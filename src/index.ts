fd203c144f9efd3ded0043f58df559348c4cde34
Jeff Kieke <2657820+i960@users.noreply.github.com>
2025-02-04 14:22:46 -0800

    Export createMat function from index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import { Mat, createMat } from './Mat';
-
-interface Params {
-    xLength: number;
-    yLength: number;
-    xSpacing: number;
-    ySpacing: number;
-    maxPieceLength?: number;
-    diameter?: number;
-    lappingFactor?: number;
-}
-
-export const calculateMat = ({ xLength, yLength, xSpacing, ySpacing, maxPieceLength = 240, diameter = 0.5, lappingFactor = 40 }: Params): Mat => {
-    return createMat({ xLength, yLength, xSpacing, ySpacing, maxPieceLength, diameter, lappingFactor });
-}
+export { Mat, createMat } from './Mat';
